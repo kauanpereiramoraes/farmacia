087e48fc5aae329dda92d6b9f2660b29b2268122
kauanpereiramoraes <106698831+kauanpereiramoraes@users.noreply.github.com>
2023-03-14 13:18:42 -0300

    Update envia.php

--- envia.php
+++ envia.php
@@ -4,10 +4,10 @@
  */
 
 // an email address that will be in the From field of the email.
-$from = 'rei.geo@hotmail.com';
+$from = 'rei.farma@hotmail.com';
 
 // an email address that will receive the email with the output of the form
-$sendo = 'oliveira.reinaldo@escola.pr.gov.br';
+$sendo = 'pereira.moraes.kauan@escola.pr.gov.br';
 
 // subject of the email
 $subject = 'Forumlário do site';
